4677beb41498a48fed12522342031423889aa089
JosueLugaro <josuelugaro15@gmail.com>
2023-10-14 23:54:03 -0500

    Added call to create genesis block, fleshed out new_block method

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,29 @@
+import hashlib
+import json
+from time import time
+from typing import Union
+
+
 class BlockChain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+    # Create the genesis block
+    self.new_block(previous_hash=1, proof=100)
 
-  def new_block(self):
+  def new_block(self, proof: int, previous_hash: Union[None, int] = None) -> dict:
     # Creates a new block and adds it to the chain
-    ...
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'transactions': self.current_transactions,
+      'proof': proof,
+      'previous_hash': previous_hash or self.hash(self.chain[-1])
+    }
+
+    self.current_transactions = []
+    self.chain.append(block)
+    return block
 
   def new_transactions(self, sender: str, recipient: str, amount: int) -> int:
     # Adds a new transaction to the list of transactions
