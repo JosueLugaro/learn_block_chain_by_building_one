668cb6b5a8c791e424b168ef58f64e936e1375c7
JosueLugaro <josuelugaro15@gmail.com>
2023-10-15 00:20:57 -0500

    Implemented proof of work algo

--- blockchain.py
+++ blockchain.py
@@ -44,3 +44,17 @@ class BlockChain(object):
     # Hashes a block
     block_string = json.dumps(block, sort_keys=True).encode()
     return hashlib.sha256(block_string).hexdigest()
+
+  def proof_of_work(self, last_proof: int) -> int:
+    proof = 0
+
+    while self.validate_proof(last_proof, proof) is False:
+      proof += 1
+
+    return proof
+
+  @staticmethod
+  def validate_proof(last_proof: int, proof: int) -> bool:
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
